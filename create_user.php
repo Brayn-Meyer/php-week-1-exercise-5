6646c0f435f5c39b775c15f5459980824f5f7ff9
Brayn-Meyer <coolmrepic9876@gmail.com>
2025-07-10 10:23:35 +0200

    completed

--- create_user.php
+++ create_user.php
@@ -4,8 +4,9 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $name = $_POST["name"];
         $email = $_POST["email"];
+        $message = $_POST["message"];
 
-        $sql = "INSERT INTO users (name, email) VALUES ('$name', '$email')";
+        $sql = "INSERT INTO users (name, email, message) VALUES ('$name', '$email', '$message')";
 
         if ($conn ->query($sql) == TRUE) {
             echo "New user has been added :(";
